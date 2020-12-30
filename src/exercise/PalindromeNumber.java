94bf0135df34a7cb64f842dd30562d97cc066fd9
Aaditya <aadi.ar1@gmail.com>
2020-12-30 14:03:09 +0400

    Modified Palindrome function without using String

--- src/exercise/PalindromeNumber.java
+++ src/exercise/PalindromeNumber.java
@@ -4,9 +4,9 @@ public class PalindromeNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		System.out.println(isPalindrome(1234));
-//		System.out.println(isPalindrome(1));
-//		System.out.println(isPalindrome(101));
+		System.out.println(isPalindrome(1234));
+		System.out.println(isPalindrome(1));
+		System.out.println(isPalindrome(101));
 		System.out.println(isPalindrome(-101));
 
 	}
@@ -14,14 +14,15 @@ public class PalindromeNumber {
 	public static boolean isPalindrome(int num) {
 		int actualValue = Math.abs(num);
 		num = Math.abs(num);
-		String lastDigit = "";
 
+		int reverse = 0;
 		while (num != 0) {
-			lastDigit += num % 10;
+			int digit = num % 10;
+			reverse = reverse * 10 + digit;
 			num = num / 10;
 		}
 
-		if (actualValue == Integer.parseInt(lastDigit)) {
+		if (actualValue == reverse) {
 			return true;
 		}
 		return false;
