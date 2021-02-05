b1c09bc69a57841aa60f4657c6555e8408b6546c
Aaditya <aadi.ar1@gmail.com>
2021-02-05 13:53:28 +0400

    Solved problem related to scope

---
+++ src/exercise/scope/Main.java
@@ -0,0 +1,14 @@
+package exercise.scope;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter a number: ");
+		X x = new X(scanner.nextInt());
+		x.x();
+		scanner.close();
+	}
+}
---
+++ src/exercise/scope/X.java
@@ -0,0 +1,17 @@
+package exercise.scope;
+
+public class X {
+
+	private int x;
+
+	public X(int x) {
+		this.x = x;
+	}
+
+	public void x() {
+		for (int x = 1; x <= 10; x++) {
+			System.out.println(x + " times " + this.x + " equals " + x * this.x);
+		}
+	}
+
+}
